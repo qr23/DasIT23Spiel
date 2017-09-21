3d5d5f028c2683be125e6559d07f9a0bd3ab1dbf
Dozent <dozent@localhost>
2017-09-21 18:34:15 +0200

    Klassen Tools und Windows angelegt

---
+++ src/Windows/Tools.java
@@ -0,0 +1,11 @@
+package Windows;
+
+public class Tools {
+	
+	public static void clear() {
+		for (int i=0;i<100;i++) {
+			System.out.println("");
+		}
+	}
+
+}
---
+++ src/Windows/Welcome.java
@@ -0,0 +1,5 @@
+package Windows;
+
+public class Welcome {
+
+}
