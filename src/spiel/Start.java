438946d2cfbc57c0352261418d4f9248df107b9d
Dozent <dozent@localhost>
2017-09-26 08:35:29 +0200

    geändert:       src/spiel/Start.java

--- src/spiel/Start.java
+++ src/spiel/Start.java
@@ -15,7 +15,7 @@ public class Start {
 	public static void main(String[] args) {
 		
 				
-		System.out.println("OS: " + System.getProperty( "os.name" ));
+		//System.out.println("OS: " + System.getProperty( "os.name" ));
 		// Intro starten
 		Intro.run();
 		
