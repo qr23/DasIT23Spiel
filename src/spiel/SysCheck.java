19d095581cd6861f3bf48dbb15b234c0721ef6e8
Dozent <dozent@localhost>
2017-09-26 13:36:15 +0200

    Systeminfos zugefügt

---
+++ src/spiel/SysCheck.java
@@ -0,0 +1,15 @@
+package spiel;
+
+import java.io.IOException;
+
+import system.SystemInfos;
+
+public class SysCheck {
+
+	public static void main(String[] args) throws IOException {
+
+		SystemInfos.show();
+
+	}
+
+}
