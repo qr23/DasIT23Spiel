13c22b99150e91d6d5cb2d20cd72e21929600501
Sebastian Pietsch <bastip92@googlemail.com>
2017-09-27 11:28:24 +0200

    Merge branch 'devBasti'
    Arbeit an Welt Array

--- src/Welt/Feld.java
+++ src/Welt/Feld.java
@@ -1,6 +1,14 @@
 package Welt;
 
+
+
 public class Feld {
+	
+	int x;
+	int y;
+	
+	boolean spieler;
+	boolean monster;
 	
 	public FeldTyp = {};
 	
--- src/Welt/Welt.java
+++ src/Welt/Welt.java
@@ -2,6 +2,10 @@ package Welt;
 
 public class Welt {
 
-	Feld karte [] [] = {{}}
+	Feld karte [][];
+	
+	karte = new Feld[10][10];
+	
+	
 	
 }
