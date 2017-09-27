5b313726447f720b72af4f98be7016f8bb866277
Julian Zier <zier.jz@gmail.com>
2017-09-27 15:59:19 +0200

    testkampf

--- src/wesen/charakter/Krieger.java
+++ src/wesen/charakter/Krieger.java
@@ -8,13 +8,13 @@ public class Krieger extends Charakter{
 		this.name = name;
 		level = 1;
 		erfahrung = 0;
-		lebenspunkte = 150;
+		lebenspunkte = 1000;
 		magiepunkte = 0;
 		staerke = 100;
 		beweglichkeit = 50;
 		intelligenz = 50;
 		angriff = staerke * 2;
-		verteidigung = 10;
+		verteidigung = 100;
 		magAtk = 0;
 		magVert = intelligenz / 25;
 	}
