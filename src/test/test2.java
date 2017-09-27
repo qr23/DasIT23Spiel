97a736ef6e0541c1536ad68b273dd406a2331f08
John Green <91hexogen@gmail.com>
2017-09-27 11:53:27 +0200

    FelderKlassen aktualisiert

--- src/test/test2.java
+++ src/test/test2.java
@@ -1,43 +1,27 @@
 package test;
 
-
-
 import io.buttons.Button;
 import io.dialog.JaNeinDialog;
 
-public class test2 {
+public class Test2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
-		JaNeinDialog d = new JaNeinDialog("Frage","Willst du ein Eis?");
+		JaNeinDialog d = new JaNeinDialog("Frage", "Willst du einen Döner");
 		
 		Button b = d.run();
+		
+		System.out.println("Key: " + b.getKey());
+		System.out.println("Name: " + b.getName());
+		System.out.println("Text: " + b.getText());
+		System.out.println("Titel: " + b.getTitle());
 
-		//System.out.println("Key: " + b.getKey());
-		//System.out.println("Name: " + b.getName());
-		//System.out.println("Text: " + b.getText());
-		//System.out.println("Title: " + b.getTitle());
 		
-		String key = b.getName();
-		
-		if (key.equalsIgnoreCase("NeinButton")) {
-			System.out.println("Oh, kein Eis?");
-		}
-		
-		if (key.equalsIgnoreCase("JaButton")) {
-			System.out.println("Oh, prima!");
-		}
-	
-		
-		
-		/*
 		if (b.getKey() == "J") {
-			System.out.println("Jarp");
+			System.out.println("Jahaaa");
 		} else {
-			System.out.println("Nope");
-		}*/
-
+			System.out.println("Neiheeeein");
+		}
 	}
 
 }
--- src/test/test3.java
+++ src/test/test3.java
@@ -3,22 +3,19 @@ package test;
 import io.buttons.Button;
 import io.dialog.Dialog;
 
-public class test3 {
+public class Test3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
-		// Dialog = Interaktion mit User
 		Dialog d = new Dialog();
 		
-		Button okButton = new Button("OK","o");
+		Button okButton = new Button("Ok", "o");
 		
 		d.addButton(okButton);
 		d.setTitel("ENDE");
-		d.setInhalt("Du bist gestorben.");
-		
+		d.setInhalt("Du hast verkackt Heiko!");
+
 		d.run();
-
 	}
-
+	
 }
