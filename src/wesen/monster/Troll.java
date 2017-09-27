8ce60e5b7c34380fec8887c3f155f863ea81a8a3
max <akaminski@gmx.de>
2017-09-27 12:23:10 +0200

    geändert:       src/wesen/monster/Troll.java

--- src/wesen/monster/Troll.java
+++ src/wesen/monster/Troll.java
@@ -1,5 +1,28 @@
 package wesen.monster;
 
-public class Troll {
+import wesen.Monster;
+
+public class Troll extends Monster{
+	
+	// Monsterkonstruktor reinholen
+	
+	public Troll(){ // Konstructormethode
+		
+		// Bei jedem aufruf passieren soll
+		this.setLebenspunkte(30);
+		this.setManapunkte(30);
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+
+	
+	
 
 }
