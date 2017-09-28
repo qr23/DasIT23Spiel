310b478a66d0cc896f407d4bc004bdfb909c7491
Dozent <dozent@localhost>
2017-09-28 13:57:27 +0200

    Inventar eingebunden

---
+++ src/gegenstand/Gegenstand.java
@@ -0,0 +1,15 @@
+package gegenstand;
+
+public class Gegenstand {
+	
+	private String name = "";
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+}
