463497eb336630d5ef0b79b8a93d2d0edc522239
Julian Zier <zier.jz@gmail.com>
2017-10-10 13:45:07 +0200

    Änderung des Hauptmenus

--- src/spiel/dialog/WillkommenDialog.java
+++ src/spiel/dialog/WillkommenDialog.java
@@ -21,14 +21,16 @@ public class WillkommenDialog extends Dialog {
 		this.addButton(quitButton);
 		
 		this.setTitel(titel);
-		this.addZeile("          888888ba                       dP d888888P   d8888b. d8888b.    .d88888b           oo          dP");
-		this.addZeile("          88    `8b                      88    88          `88     `88    88.     '                      88");
-		this.addZeile("          88     88 .d8888b. .d8888b.    88    88       .aaadP'  aaad8'   `Y88888b. 88d888b. dP .d8888b. 88");
-		this.addZeile("          88     88 88'  `88 Y8ooooo.    88    88      88'         `88          `8b 88'  `88 88 88ooood8 88");
-		this.addZeile("          88    .8P 88.  .88       88    88    88      88.         .88     d8'  .8P 88.  .88 88 88.  ... 88");
-		this.addZeile("          8888888P  `88888P8 `88888P'    dP    dP      Y88888P d88888P     Y88888P  88Y888P' dP `88888P' dP");
-		this.addZeile("                                                                                    88                     ");
-		this.addZeile("                                                                                    dp                     ");
+		this.addZeile("");
+		this.addZeile("           888888ba                       dP d888888P   d8888b. d8888b.    .d88888b           oo          dP");
+		this.addZeile("           88    `8b                      88    88          `88     `88    88.     '                      88");
+		this.addZeile("           88     88 .d8888b. .d8888b.    88    88       .aaadP'  aaad8'   `Y88888b. 88d888b. dP .d8888b. 88");
+		this.addZeile("           88     88 88'  `88 Y8ooooo.    88    88      88'         `88          `8b 88'  `88 88 88ooood8 88");
+		this.addZeile("           88    .8P 88.  .88       88    88    88      88.         .88     d8'  .8P 88.  .88 88 88.  ... 88");
+		this.addZeile("           8888888P  `88888P8 `88888P'    dP    dP      Y88888P d88888P     Y88888P  88Y888P' dP `88888P' dP");
+		this.addZeile("                                                                                     88                     ");
+		this.addZeile("                                                                                     dp                     ");
+		this.addZeile("");
 	}
 
 }
