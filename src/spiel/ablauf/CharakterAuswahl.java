8d765d8a3d879dee92b3dc9031ab30838f1f50a6
Dozent <dozent@localhost>
2017-10-11 16:09:20 +0200

    Update

--- src/spiel/ablauf/CharakterAuswahl.java
+++ src/spiel/ablauf/CharakterAuswahl.java
@@ -1,10 +1,14 @@
 package spiel.ablauf;
 
 import io.buttons.Button;
-import io.buttons.ZurueckButton;
 import io.dialog.EingabeDialog;
 import io.dialog.JaNeinDialog;
+import spiel.Spiel;
 import spiel.dialog.CharakterAuswahlDialog;
+import wesen.Charakter;
+import wesen.charakter.Krieger;
+import wesen.charakter.Magier;
+import wesen.charakter.Schurke;
 
 public class CharakterAuswahl {
 	
@@ -67,8 +71,59 @@ public class CharakterAuswahl {
 		
 		System.out.println("Name: " + name);
 		
+		Charakter charakter = null;
 		
-		//ToDo: CharakterObjekt zurückgeben!
+		//Je nach Auswahl ein CharakterObjekt erstellen ... 
+		if (antwort1.is("Ritter")) { charakter = new Krieger(name); }
+		if (antwort1.is("Magier")) { charakter = new Magier(name); }
+		if (antwort1.is("Schurke")) { charakter = new Schurke(name); }
+		
+		// ... und speichern
+		Spiel.setCharakter(charakter);
+		
+		
+	}
+	
+	private static void charakterDialogAnzeigen() {
+		Button antwort;		
+
+		do {
+			// Welcher Charakter wird gewählt? 
+			// Erzeugen und starten des CharakterAuswahlDialogs
+			CharakterAuswahlDialog charAusDialog = new CharakterAuswahlDialog();
+			antwort = charAusDialog.run();
+			
+			// Hinweis
+			// -------
+			// im Button 'antwort1' befindet sich der 
+			// Button, auf den im Dialogfeld geklickt wurde.
+			// Der muss jetzt ausgewertet werden.
+			
+			// bekommt einen individuellen InfoText zur Anzeige.
+			String infoText = "";
+			
+			// Je nach Auswahl des Charakters den InfoText setzen
+			if (antwort.is("Ritter")) {infoText = "einen Ritter";}
+			if (antwort.is("Magier")) {infoText = "einen Magier";}
+			if (antwort.is("Schurke")) {infoText = "einen Schurken";}
+			
+			// Ist der ZurückButton gedrückt, Intro starten
+			if (antwort.is("ZurückButton")) {Intro.run();}
+		
+			dialog02();
+			
+		} while ();
+	}
+	
+	private static void dialog02() {
+		// AnzeigeText zusammen bauen.
+		String text = "Du hast Dir " + infoText + " ausgewählt. "
+					+ "Willst Du dabei bleiben?";
+					
+		// JaNeinDialog erzeugen und starten.
+		JaNeinDialog frage = new JaNeinDialog("Charakterwahl", text);
+		antwort2 = frage.run();
+		
 	}
 
 }
